1fa010da68537f34c6581b435e5628126fd97217
ChaeyeonHan <kate2236e@naver.com>
2022-07-20 22:19:50 +0900

    좋아요/좋아요 취소 API 구현

---
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,48 @@
+package com.cos.photogramstart.domain.likes;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes_uk",
+                        columnNames = {"imageId", "userId"}  // 동일한 이미지에 좋아요 여러번X
+                )
+        }
+)
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne
+    private Image image;
+
+    // 오류가 나면 잡아보기
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+
+}
