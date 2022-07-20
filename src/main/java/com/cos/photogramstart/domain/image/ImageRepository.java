4e701019616f64afa773d042c71ac70e818116b0
ChaeyeonHan <kate2236e@naver.com>
2022-07-20 15:01:59 +0900

    뷰 렌더링 및 페이징 처리

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -7,6 +9,6 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-    @Query(value = "SELECT * FROM image WHERE userId in (SELECT toUserId FROM subscribe WHERE fromUserID = :principalId);", nativeQuery = true)
-    List<Image> mStory(int principalId);
+    @Query(value = "SELECT * FROM image WHERE userId in (SELECT toUserId FROM subscribe WHERE fromUserID = :principalId) ORDER BY id DESC", nativeQuery = true)
+    Page<Image> mStory(int principalId, Pageable pageable);  // pageable넣어주면 알아서 정렬해서 가져온다
 }
--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -6,6 +6,8 @@ import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -53,8 +55,8 @@ public class ImageService {
     }
 
     @Transactional(readOnly = true)  // 영속성 컨텍스트 변경감지를 해서, 변경이 되었으면 더티체킹을해서 db에 flush(반영) X -> 성능이 좀 더 좋아짐
-    public List<Image> 이미지스토리(int principalId){
-        List<Image> images = imageRepository.mStory(principalId);
+    public Page<Image> 이미지스토리(int principalId, Pageable pageable){
+        Page<Image> images = imageRepository.mStory(principalId, pageable);
         return images;
     }
 }
